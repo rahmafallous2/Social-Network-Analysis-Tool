45e9dd6ec77feca26e23150454c96c7a6ba479f1
Your Name <you@example.com>
2024-07-10 01:11:10 +0300

    Implement method to add friends in User class

---
+++ class-user.py
@@ -0,0 +1,17 @@
+#Social Network Analysis Tool Project
+#1. Graph Representation:
+# I chose adjacency list for sparse graph representation due to the efficiency in memory usage and traversal speed, ideal for graphs with relatively few edges compared to maximum possible connections between users.
+class User:
+    def __init__(self, id, name, age, email, phone_number, list_friends):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.email = email
+        self.phone_number = phone_number
+        self.list_friends = list_friends
+    def addUser(self, user):
+        if user.id not in [friend.id for friend in self.list_friends]:
+            self.list_friends.append(user)
+        else:
+            print("The user already is your friend!")
+
